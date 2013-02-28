464b577eafab0113477b2f14bb3a100fc0b4f0f7
Ben Flowers <ben@DeWayne.(none)>
2013-02-28 16:57:21 +0000

    adding unit tests around product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: false,format:{
+  	with: /\.(gif|jpg|png)/,
+  	message: 'Muse be gif, png, or jpg'
+    }
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,61 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "product must have title"  do
+		product = Product.create(MockProductHashBuilder.build_without(:title))
+		assert_equal true, product.errors[:title].any?
+	end
+
+	test "product must have description"  do
+		product = Product.create(MockProductHashBuilder.build_without(:description))
+		assert_equal true, product.errors[:description].any?
+	end
+
+	test "product must have image_url"  do
+		product = Product.create(MockProductHashBuilder.build_without(:image_url))
+		assert_equal true, product.errors[:image_url].any?
+	end
+
+	test "product price must be more than 0.00" do
+		product = Product.create(MockProductHashBuilder.build_with(:price => 0.00))
+		assert_equal true, product.errors[:price].any?
+	end
+
+	test "product image_url accepts png images" do
+		product = Product.create(MockProductHashBuilder.build_with(:image_url => 'image.png'))
+		assert_equal false, product.errors[:image_url].any?
+	end
+
+	test "product image_url accepts jpg images" do
+		product = Product.create(MockProductHashBuilder.build_with(:image_url => 'image.jpg'))
+		assert_equal false, product.errors[:image_url].any?
+	end
+
+	test "product image_url accepts gif images" do
+		product = Product.create(MockProductHashBuilder.build_with(:image_url => 'image.jpg'))
+		assert_equal false, product.errors[:image_url].any?
+	end
+
+	test "product image_url accepts rejects non-correct image format" do
+		product = Product.create(MockProductHashBuilder.build_with(:image_url => 'imagepg'))
+		assert_equal true, product.errors[:image_url].any?
+	end
 end
+
+class MockProductHashBuilder
+	
+	def self.build_without(key)
+		product = { title: "Hello", description: "world", image_url: "image.png", price: 0.01}
+		product[key] = nil
+		return product
+	end
+
+	def self.build_with(hash)
+		product = { title: "Hello", description: "world", image_url: "image.png", price: 0.01}
+		hash.each do |key,value|
+			product[key] = value
+		end
+		return product
+	end
+end
+
