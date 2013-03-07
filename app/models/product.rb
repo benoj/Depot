d7b1657cd2b4653d88ec816423bf8cbddd4567c1
Ben Uni <benuni@DeShawn.(none)>
2013-03-07 20:28:06 +0000

    adding basic card functionality to the Depot application

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item	
+ 
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
@@ -7,4 +10,15 @@ class Product < ActiveRecord::Base
   	with: /\.(gif|jpg|png)/,
   	message: 'Muse be gif, png, or jpg'
     }
+
+    private 
+    def ensure_not_referenced_by_any_line_item
+    	if line_items.empty?
+    		return true
+    	else
+    		errors.add(:base, 'Line Items present')
+    		return false
+    	end
+    end
+
 end
